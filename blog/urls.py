8ef41559493992716b4a634eab8359746e7cc20e
jhojan <jhojan@example.com>
2025-03-11 17:35:45 -0400

    publication detail done

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PublicationListView
+from .views import PublicationListView, PublicationDetailView
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='publications-list'),
+    path('Publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'),
 ]
    
