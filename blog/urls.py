faeb2e623ad14794da62bc9ee7acff0a0f56ae2a
jhojan <jhojan@example.com>
2025-03-11 18:07:28 -0400

    publication create v1 without

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import PublicationListView, PublicationDetailView
+from .views import (
+                        PublicationListView, 
+                        PublicationDetailView,
+                        PublicationCreateView,
+                    )
+
 
 urlpatterns = [
     path('', PublicationListView.as_view(), name='publications-list'),
     path('Publication/<int:pk>/', PublicationDetailView.as_view(), name='publication-detail'),
+    path('Publication/new/', PublicationCreateView.as_view(), name='publication-create'),
 ]
    
